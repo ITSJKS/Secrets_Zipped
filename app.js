554d063f23bdb1fb386c76bbc3abf2cd683ac79c
JItesh Kumar Singh <75828914+ITSJKS@users.noreply.github.com>
2022-06-30 20:33:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,158 @@
+//jshint e sversion:6
+require('dotenv').config();
+const express  = require('express');
+const bodyParser = require('body-parser');
+const ejs = express('ejs');
+const mongoose = require('mongoose');
+const app = express();
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+  secret:"Our little secret.",
+  resave:false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect("mongodb://localhost:27017/userDB");
+const userSchema = mongoose.Schema({
+  email:String,
+  password:String,
+  googleId : String,
+  secret : String
+});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+const User = new mongoose.model("user",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/",(req,res)=>{
+  res.render("home");
+})
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/secrets');
+  });
+app.get("/login",(req,res)=>{
+  res.render("login");
+})
+app.get("/register",(req,res)=>{
+  res.render("register");
+})
+
+app.get("/secrets",(req,res)=>{
+  User.find({"secret":{$ne:null}},(err,foundUsers)=>{
+    if(err) console.log(err);
+    else{
+      if(foundUsers){
+        res.render("secrets",{usersWithSecrets:foundUsers});
+      }
+    }
+  })
+  }
+)
+app.get("/submit",(req,res)=>{
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }
+  else{
+    res.redirect("/login");
+  }
+})
+app.post("/submit",(req,res)=>{
+  const submittedSecret = req.body.secret;
+  // console.log(req.user.id);
+  User.findById(req.user.id,(err,foundUser)=>{
+    if(err) console.log(err);
+    else{
+      if(foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(()=>{res.redirect("secrets")});
+      }
+    }
+  })
+})
+
+app.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+app.post("/register",(req,res)=>{
+User.register({username:req.body.username},req.body.password,(err,user)=>{
+  if(!err){
+    passport.authenticate("local")(req,res,()=>{
+      res.redirect("/secrets");
+    })
+    }
+  else{
+    console.log(err);
+    res.redirect("/");
+  }
+})
+})
+
+app.post("/login",(req,res)=>{
+const user = new User({
+  username:req.body.username,
+  password:req.body.password
+})
+req.login(user,(err)=>{
+  if(err){
+    console.log(err);
+    res.redirect("/login");
+  }
+  else{
+    passport.authenticate("local")(req,res,()=>{
+      res.redirect("/secrets");
+    })
+  }
+})
+})
+
+
+
+
+app.listen(3000,(err)=>{
+  if(!err) console.log("Server started on Port 3000");
+  else{
+    console.log(err);
+  }
+})
